e4dbb0f7ed6e2ad287d85c5bbd61e57929495948
Shallyn <shallyn.liu@foxmail.com>
2019-08-25 10:28:21 +0800

    8_21 update

--- _core/skymap/__init__.py
+++ _core/skymap/__init__.py
@@ -3,7 +3,23 @@ This is the module for gravitational wave coherent search.
 Writer: Shallyn(shallyn.liu@foxmail.com)
 """
 
-from .pix import nside2npix, pix2ang, 
+from .pix import nside2npix, pix2ang
 from .skymap import mollview, graticule, MollweideProj
 
-__all__ = ['nside2npix', 'pix2ang', 'mollview', 'graticule', 'MollweideProj']
+__all__ = ['nside2npix', 
+           'pix2ang', 
+           'mollview', 
+           'graticule', 
+           'MollweideProj',
+           'Skymap']
+
+
+class Skymap(object):
+    def __init__(self, utdk2):
+        self._coh_snr_pix = np.sqrt(np.sum(utdk2[:,:,:2], axis = 2).max(axis=0))
+        if utdk2.shape[2] > 2:
+            null = np.sum(utdk2[:,:,2:], axis=2)
+            null_snr2 = null.sum(axis=0) / utdk2.shape[0]
+            self._null_snr_pix = np.sqrt(null_snr2)
+        
+
