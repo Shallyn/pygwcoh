7d28ad99d21e12949006fee54367e718d5a2579a
Shallyn <shallyn.liu@foxmail.com>
2019-08-25 10:52:45 +0800

    8_21 update

--- exe.py
+++ exe.py
@@ -44,7 +44,7 @@ def parseargs(argv):
     parser.add_option('--m2', type = 'float', help = 'mass2 of this event, for template generation.')
     parser.add_option('--s1z', type = 'float', help = 'spin1z of this event, for template generation.')
     parser.add_option('--s2z', type = 'float', help = 'spin2z of this event, for template generation.')
-    parser.add_option('--fini', type = 'float', default = 0.001, help = 'Initial frequency for template generation(natual dimension).')
+    parser.add_option('--fini', type = 'float', default = 20, help = 'Initial frequency for template generation(natual dimension).')
     parser.add_option('--approx', type = 'str', help = 'approx for template generation.')
 
     # Localize 
