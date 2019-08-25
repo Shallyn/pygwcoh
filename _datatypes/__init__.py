03844e301e37fb1bb59fde4ca317dd9946b24c1e
Shallyn <shallyn.liu@foxmail.com>
2019-08-26 05:14:45 +0800

    8_21 update

--- _datatypes/__init__.py
+++ _datatypes/__init__.py
@@ -3,12 +3,11 @@ This is the module for gravitational wave coherent search.
 Writer: Shallyn(shallyn.liu@foxmail.com)
 """
 
-from .series import TimeSeries, TimeFreqSpectrum, CreateEmptySpectrum
+from .series import TimeSeries, TimeFreqSpectrum
 from .strain import gwStrain
 from .detector import Detector
 
 __all__ = ['TimeSeries',
            'gwStrain',
            'Detector',
-           'TimeFreqSpectrum',
-           'CreateEmptySpectrum']
+           'TimeFreqSpectrum']
