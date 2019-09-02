263951daa9efa2843fb94c650a64139afa29a879
Shallyn <shallyn.liu@foxmail.com>
2019-09-02 13:53:58 +0800

    8_30 update

--- _coherent.py
+++ _coherent.py
@@ -66,7 +66,7 @@ class gwStrainCoherent(object):
             C2H_corr += (corr_N / np.sqrt(corr_H))**2
         distance = -C_noise / H_corr + \
             np.sqrt(4*(C_noise / H_corr)**2 - (C2H_corr - snr_expected**2) / H_corr )
-        LOGGER.warning(f'distance = {distance}\n')
+        LOGGER.info(f'rescaled distance factor = {tmpl.distance * distance} Mpc\n')
         for strain in self:
             strain.make_injection(tmpl, gps, ra_inj, de_inj, distance, 
                         psi = psi, phic = phic)
